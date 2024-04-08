0663cb8260ff29083aa5620f30fbf5a0fd8bd7d5
Alud24k <ifem.perez@gmail.com>
2024-04-08 20:43:27 -0300

    Se agrego el componente Informacion.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 function App() {
-  return <h1>Prueba</h1>;
+  return (
+    <main className="container my-5">
+      <h1>Prueba</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam eum
+        recusandae totam provident animi impedit at, obcaecati soluta autem
+        architecto, maxime doloribus perferendis? Deleniti est excepturi
+        laboriosam quae delectus reiciendis.
+      </p>
+    </main>
+  );
 }
 
 export default App;
