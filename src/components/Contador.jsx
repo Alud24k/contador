b5c73e51770af67922338a5a1a4a0a15575aa9a5
Alud24k <ifem.perez@gmail.com>
2024-04-10 20:44:07 -0300

    Se agrego el state de restar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,19 +1,22 @@
-const Contador = () => {
-  let numero = 0;
+import { useState } from "react";
 
-  const sumar = () => {
-    numero++;
-    console.log(numero);
-  };
+const Contador = () => {
+  // let numero = 0;
+  const [numero, setNumero] = useState(0);
 
   return (
     <section className="text-center">
       <h2>Contador</h2>
       <p>{numero}</p>
-      <button className="btn btn-primary me-2" onClick={sumar}>
+      <button
+        className="btn btn-primary me-2"
+        onClick={() => setNumero(numero + 1)}
+      >
         +1
       </button>
-      <button className="btn btn-danger">-1</button>
+      <button className="btn btn-danger" onClick={() => setNumero(numero - 1)}>
+        -1
+      </button>
     </section>
   );
 };
